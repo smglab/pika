04f15c20a275d6fc5195451b186736ce40ff9193
smglab Max Shabalihin <shabalihin@gmail.com>
2012-09-22 15:51:19 +0400

    fix blocking connect timeout and socket_timeout check

--- pika/connection.py
+++ pika/connection.py
@@ -145,7 +145,7 @@ class ConnectionParameters(object):
             raise TypeError("retry_delay must be an int")
 
         # Validate the socket timeout delay
-        if not isinstance(socket_timeout, int):
+        if not isinstance(socket_timeout, int) or not isinstance(socket_timeout, types.NoneType):
             raise TypeError("socket_timeout must be an int")
 
         # Assign our values
